a2f7830dc802b5ccc0601bb0879e9ea21129cad7
AllrightIsHere <gasilva912@gmail.com>
2024-08-10 14:39:55 -0300

    chore: rename Aplication

--- src/main/java/com/videocustom/videocustom/Application.java
+++ src/main/java/com/videocustom/videocustom/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideocustomApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideocustomApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
