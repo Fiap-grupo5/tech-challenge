7976765c13524620ff3ecd54c70cd9db6b6dde25
Rafael-Cerqueira <rafael.scerqueira@hotmail.com>
2024-08-13 16:34:00 -0300

    Feat: Hexagonal architecture login

--- src/main/java/com/videocustom/videocustom/application/repositories/LoginRepository.java
+++ src/main/java/com/videocustom/videocustom/application/repositories/LoginRepository.java
@@ -1,2 +1,12 @@
-package com.videocustom.videocustom.application.repositories;public class LoginRepository {
+package com.videocustom.videocustom.application.repositories;
+
+import com.videocustom.videocustom.application.domain.user.User;
+import com.videocustom.videocustom.application.domain.base.BaseId;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LoginRepository {
+
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
--- src/main/java/com/videocustom/videocustom/application/usecases/login/LoginUseCaseOutput.java
+++ src/main/java/com/videocustom/videocustom/application/usecases/login/LoginUseCaseOutput.java
@@ -1,2 +1,27 @@
-package com.videocustom.videocustom.application.usecases.login;public class LoginUseCaseOutput {
+package com.videocustom.videocustom.application.usecases.login;
+
+import com.videocustom.videocustom.application.domain.user.User;
+
+import java.time.Instant;
+
+public record LoginUseCaseOutput(
+        String id,
+        String name,
+        String email,
+        boolean isGuest,
+        Instant createdAt,
+        Instant updatedAt,
+        Instant deletedAt
+) {
+    public static LoginUseCaseOutput from(User user) {
+        return new LoginUseCaseOutput(
+                user.getId().value(),
+                user.getName(),
+                user.getEmail(),
+                false,
+                user.getCreatedAt(),
+                user.getUpdatedAt(),
+                user.getDeletedAt()
+        );
+    }
 }
