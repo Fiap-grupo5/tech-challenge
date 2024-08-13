5d57d64cdec4258de364581e71744993eb1c7023
Jessica Maria <jmd.sants@gmail.com>
2024-08-13 10:45:49 -0300

    refactor: ajuste na chamada principal application

--- src/main/java/com/videocustom/Application.java
+++ src/main/java/com/videocustom/Application.java
@@ -1,6 +1,5 @@
 package com.videocustom;
 
-import com.videocustom.controller.project.ProjectController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,4 +10,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+
+	}
 }
